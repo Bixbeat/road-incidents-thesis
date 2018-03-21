71a877ca9b35f26d04448b122f981185607fd631
bixbeat <ahlevering@hotmail.com>
2018-03-21 15:59:20 +1100

    Added saving to Flickr API caller

--- lib/scraping/img_handler.py
+++ lib/scraping/img_handler.py
@@ -1,23 +1,13 @@
+import io
 import os.path
-import shutil
-
-class Handler():
-    def __init__(self, data_folder='data'):
-        self.root = data_folder
-        self.query_log = 'queries'
-        
-    def store_query(self,query, id_num):
-        query_path = os.path.join(self.root, 'log', self.query_log)
-        if not os.path.isfile(query_path):mode = 'w'
-        else: mode = 'a'
-        
-        with open(query_path, mode) as file:
-            if mode == 'w': file.write("id_num,query\n")
-            file.write(f"{id_num}, {self.query_log}\n")
-            
-    def save_pil_image(self, image, path):
-        pass
+import urllib
+from PIL import Image
 
 def write_img_from_url(image, path):
-    with open(path, 'wb') as out_file:
-        shutil.copyfileobj(image.raw, out_file)
+    with io.BytesIO(image.content) as f:
+        with Image.open(f) as img:
+            img.save(path)
+
+def create_dir_if_not_exist(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
