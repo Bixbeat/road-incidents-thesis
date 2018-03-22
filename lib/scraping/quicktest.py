3fd1f256bdcf72151893d4d553b6e3511e797a9d
bixbeat <ahlevering@hotmail.com>
2018-03-22 17:22:19 +1100

    Minor structure cleanup

--- lib/scraping/quicktest.py
+++
@@ -1,7 +0,0 @@
-import shutil
-import urllib
-
-url = 'https://farm5.staticflickr.com/4793/39830059435_e3c1cb2df0_o.jpg'
-response = requests.get(url, stream=True)
-with open('/home/alex/Documents/Scripts/road-incidents-thesis/lib/scraping/img.png', 'wb') as out_file:
-    shutil.copyfileobj(response.raw, out_file)
