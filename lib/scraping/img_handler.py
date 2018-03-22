e29d3b230828b3c3e375a19b016e88c7b66d1a84
bixbeat <ahlevering@hotmail.com>
2018-03-22 14:55:53 +1100

    Added pickle storage of API responses

--- lib/scraping/img_handler.py
+++ lib/scraping/img_handler.py
@@ -1,6 +1,7 @@
 import io
 import os.path
 import urllib
+import pickle
 from PIL import Image
 
 def write_img_from_url(image, path):
@@ -11,3 +12,14 @@ def write_img_from_url(image, path):
 def create_dir_if_not_exist(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+def construct_output_dir(data_root, search_grouping, tags, source):
+    return(os.path.join(data_root, search_grouping, tags))
+
+def store_response(response, pickle_file):
+    with open(pickle_file, 'wb') as p:
+        pickle.dump(response, p, protocol=pickle.HIGHEST_PROTOCOL)
+
+def assert_offset(page, imgs_per_req):
+    assert(page >= 0)
+    return(page * imgs_per_req)
